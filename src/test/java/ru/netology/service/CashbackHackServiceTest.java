7ec5b0e15a7c4e05894571107794a45c010d4438
Ярослав Черников <yaroslavcher@gmail.com>
2022-08-07 15:39:28 +0300

    initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn1() {
+        assertEquals(1, service.remain(999));
+    }
+
+    @Test
+    public void shouldReturn999() {
+        assertEquals(999, service.remain(1001));
+    }
+
+    @Test
+    public void shouldReturn0() {
+        assertEquals(1000, service.remain(1000));
+    }
+}
